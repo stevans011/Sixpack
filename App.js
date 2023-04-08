aec8c3536e3b9ea22fa49e744a442850e639846c
stevans011 <stevanoffs@gmail.com>
2023-04-08 19:34:05 +1000

    First Commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StyleSheet, useColorScheme, Text } from "react-native";
+// import NavigationStack from "./src/navigation/NavigationStack";
+import { Provider } from "react-redux";
+// import { store } from "./src/redux/store";
+import Toast from "react-native-toast-message";
+import { StatusBar } from "react-native";
+
+export default function App() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <>
+      <StatusBar
+        backgroundColor="transparent"
+        barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+      />
+      {/* <Provider store={store}><NavigationStack /></Provider> */}
+      <Text>Welcome to Cross App</Text>
+      <Toast />
+    </>
+  );
+}
